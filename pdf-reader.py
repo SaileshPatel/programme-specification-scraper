0fc47dd39340be0d0b6d4da35c8bf8af5f363035
Sailesh Patel <patelsa3@aston.ac.uk>
2019-10-06 16:43:04 +0100

    add additional line to convert file to CSV

--- pdf-reader.py
+++ pdf-reader.py
@@ -4,5 +4,7 @@ def extract_text(path):
     pdf_text = tabula.read_pdf(path)
     print(pdf_text) # print pandas.core.frame.DataFrame
 
+    tabula.convert_into(path, 'pdf_csv_file.csv')
+
 if __name__ == '__main__':
     extract_text('../ProgrammeSpecifications/BScComputerScience.pdf')
