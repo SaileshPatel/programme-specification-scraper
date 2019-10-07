acc927db759e2232f51a14103867c7d935d936fc
Sailesh Patel <patelsa3@aston.ac.uk>
2019-10-07 13:44:54 +0100

    add reference to code

--- pdf-reader.py
+++ pdf-reader.py
@@ -1,3 +1,10 @@
+# - Author: Kaushik Acharya (https://github.com/kaushikacharya)
+# - Date: 24/03/2014
+# - Title: Programming with PDFMiner
+# - Type: Programming Library Documentation
+# - Availability: http://www.unixuser.org/~euske/python/pdfminer/programming.html
+
+
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
