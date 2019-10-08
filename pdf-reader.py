b8a85a87f14c7903e9c6c0d3b9c1b7dcff408bcd
Sailesh Patel <patelsa3@aston.ac.uk>
2019-10-08 21:03:25 +0100

    add lattice to prevent new lines being treated as new rows

--- pdf-reader.py
+++ pdf-reader.py
@@ -1,12 +1,12 @@
 import tabula
 
 def extract_text(path):
-    pdf_text = tabula.read_pdf(path)
+    pdf_text = tabula.read_pdf(path, lattice=True)
     print(pdf_text) # print pandas.core.frame.DataFrame
 
-    tabula.convert_into(path, 'pdf_tsv_file.tsv', 'tsv')
-    tabula.convert_into(path, 'pdf_json_file.json', 'json')
-    tabula.convert_into(path, 'pdf_csv_file.csv')
+    tabula.convert_into(path, 'pdf_tsv_file.tsv', 'tsv', lattice=True)
+    tabula.convert_into(path, 'pdf_json_file.json', 'json', lattice=True)
+    tabula.convert_into(path, 'pdf_csv_file.csv', lattice=True)
 
 if __name__ == '__main__':
     extract_text('../ProgrammeSpecifications/BScComputerScience.pdf')
