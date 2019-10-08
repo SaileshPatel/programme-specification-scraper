fd4b10fca26e8fe4d10eb1cfdb0df85e9124a086
Sailesh Patel <patelsa3@aston.ac.uk>
2019-10-08 22:00:46 +0100

    rename and refactor extraction function
    resolves #4

--- pdf-reader.py
+++ pdf-reader.py
@@ -1,14 +1,18 @@
 import tabula
 
-def extract_text(path):
+def extract_course_info(course):
+
+    path = '../ProgrammeSpecifications/' + spec_to_get + ".pdf"
+    filename = 'pdf_' + spec_to_get
+
     # pdf_text = tabula.read_pdf(path, lattice=True)
     # print(pdf_text) # print pandas.core.frame.DataFrame
     
-    tabula.convert_into(path, 'pdf_tsv_file.tsv', 'tsv', lattice=True)
-    tabula.convert_into(path, 'pdf_json_file.json', 'json', lattice=True)
-    tabula.convert_into(path, 'pdf_csv_file.csv', lattice=True)
+    tabula.convert_into(path, filename + '.tsv', 'tsv', lattice=True)
+    tabula.convert_into(path, filename + '.json', 'json', lattice=True)
+    tabula.convert_into(path, filename + '.csv', lattice=True)
 
 if __name__ == '__main__':
     spec_to_get = input("Enter in the course specification you would like to scrape: ")
     # BScComputerScience.pdf
-    extract_text('../ProgrammeSpecifications/' + spec_to_get + ".pdf")
+    extract_course_info(spec_to_get)
