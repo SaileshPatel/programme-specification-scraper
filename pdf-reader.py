a274db9feab1c39fc18617d13406ba4ebdbe74a1
Sailesh Patel <patelsa3@aston.ac.uk>
2019-10-08 20:12:09 +0100

    try json and tsv extraction of PDF information

--- pdf-reader.py
+++ pdf-reader.py
@@ -4,6 +4,8 @@ def extract_text(path):
     pdf_text = tabula.read_pdf(path)
     print(pdf_text) # print pandas.core.frame.DataFrame
 
+    tabula.convert_into(path, 'pdf_tsv_file.tsv', 'tsv')
+    tabula.convert_into(path, 'pdf_json_file.json', 'json')
     tabula.convert_into(path, 'pdf_csv_file.csv')
 
 if __name__ == '__main__':
