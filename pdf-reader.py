fe6a19df1498f1ef3dae658e097f0aadd96e334d
Sailesh Patel <patelsa3@aston.ac.uk>
2019-10-08 21:33:11 +0100

    comment out unneeded printing statements

--- pdf-reader.py
+++ pdf-reader.py
@@ -1,9 +1,9 @@
 import tabula
 
 def extract_text(path):
-    pdf_text = tabula.read_pdf(path, lattice=True)
-    print(pdf_text) # print pandas.core.frame.DataFrame
-
+    # pdf_text = tabula.read_pdf(path, lattice=True)
+    # print(pdf_text) # print pandas.core.frame.DataFrame
+    
     tabula.convert_into(path, 'pdf_tsv_file.tsv', 'tsv', lattice=True)
     tabula.convert_into(path, 'pdf_json_file.json', 'json', lattice=True)
     tabula.convert_into(path, 'pdf_csv_file.csv', lattice=True)
