40bbedc92f8d63a73dd4a039dcf4c64e715cefa3
Sailesh Patel <patelsa3@aston.ac.uk>
2019-10-10 22:12:32 +0100

    import panda data structure library

--- pdf-reader.py
+++ pdf-reader.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import tabula
 
 def extract_course_info(course):
@@ -5,14 +6,14 @@ def extract_course_info(course):
     path = '../ProgrammeSpecifications/' + spec_to_get + ".pdf"
     filename = 'pdf_' + spec_to_get
 
-    # pdf_text = tabula.read_pdf(path, lattice=True)
-    # print(pdf_text) # print pandas.core.frame.DataFrame
+    pdf_text = tabula.read_pdf(path, lattice=True)
+    print(pdf_text) # print pandas.core.frame.DataFrame
     
-    tabula.convert_into(path, filename + '.tsv', 'tsv', lattice=True)
-    tabula.convert_into(path, filename + '.json', 'json', lattice=True)
-    tabula.convert_into(path, filename + '.csv', lattice=True)
+    # tabula.convert_into(path, filename + '.tsv', 'tsv', lattice=True)
+    # tabula.convert_into(path, filename + '.json', 'json', lattice=True)
+    # tabula.convert_into(path, filename + '.csv', lattice=True)
 
 if __name__ == '__main__':
     spec_to_get = input("Enter in the course specification you would like to scrape: ")
-    # BScComputerScience.pdf
+    # BScComputerScience
     extract_course_info(spec_to_get)
