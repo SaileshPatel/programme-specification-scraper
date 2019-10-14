4d6c2c79874a381b9a341fc75766f6bf549f6741
Sailesh Patel <patelsa3@aston.ac.uk>
2019-10-14 12:19:16 +0100

    reformat programme delivery and attendance type data

--- pdf-reader.py
+++ pdf-reader.py
@@ -15,13 +15,25 @@ def extract_course_info(course):
     specification_data['Location'] = pdf_text.iloc[9, 1]
     specification_data['Programme Length'] = pdf_text.iloc[10, 1]
     specification_data['Total Credits'] = pdf_text.iloc[11, 1]
+    specification_data['Accredited'] = pdf_text.iloc[12, 1]
 
     #print(type(pdf_text.iloc[6]))
 
-    # loop over
-        # if value = x, 
-            # get index 
-            # depending on index, give answer
+    for index, value in pdf_text.iloc[6].items():
+        if value == 'X':
+            if index == 1:
+                specification_data['Attendance Type'] = "Full Time"
+            elif index == 2:
+                specification_data['Attendance Type'] = "Part Time"
+
+    for index, value in pdf_text.iloc[8].items():
+        if value == 'X':
+            if index == 1:
+                specification_data['Delivery Pattern'] = "Campus-based"
+            elif index == 2:
+                specification_data['Delivery Pattern'] = "Work-based"
+            elif index == 3:
+                specification_data['Delivery Pattern'] = "Online/distance"
 
 
     print(specification_data)
