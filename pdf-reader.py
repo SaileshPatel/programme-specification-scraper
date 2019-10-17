3e79a5492ba52ca19eb57c540174600ce9c3d78f
Sailesh Patel <patelsa3@aston.ac.uk>
2019-10-17 22:38:49 +0100

    add check to see if file can be parsed with tabula

--- pdf-reader.py
+++ pdf-reader.py
@@ -9,8 +9,12 @@ def extract_course_info(course):
 
     pdf_text = tabula.read_pdf(path, lattice=True, pandas_options={'header': None}, pages="all", multiple_tables=True)
 
-    for pages in pdf_text:
-        print(pages)
+
+    if(isinstance(pdf_text[0].iloc[0, 0], str)):
+        print(pdf_text[0])
+    else:
+        print("Need to parse differently")
+        
 
 if __name__ == '__main__':
     spec_to_get = input("Enter in the course specification you would like to scrape: ")
