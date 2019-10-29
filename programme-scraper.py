540beb4cae261ecc188667ac0a4fb4a4dc906d30
Sailesh Patel <patelsa3@aston.ac.uk>
2019-10-29 20:18:56 +0000

    remove PDFToText usage

--- programme-scraper.py
+++ programme-scraper.py
@@ -11,53 +11,34 @@ def extract_course_info(course):
     
     specification_data = {}
 
-    if(isinstance(pdf_text[0].iloc[0, 0], str)):
-        for index, page in enumerate(pdf_text):
-            # page = page in PDF file
-            # index = page no i.e 0 = 1, 1 = 2, etc
-            if(index == 0):
-                specification_data[page.iloc[0, 0]] = page.iloc[0, 1] # Programe Title
-                specification_data['School of Study'] = page.iloc[2, 1]
-                specification_data['Degree Type'] = page.iloc[3, 1]
-                specification_data['Location'] = page.iloc[9, 1]
-                specification_data['Programme Length'] = page.iloc[10, 1]
-                specification_data['Total Credits'] = page.iloc[11, 1]
-                specification_data['Accredited'] = page.iloc[12, 1]
+    for index, page in enumerate(pdf_text):
+        # page = page in PDF file
+        # index = page no i.e 0 = 1, 1 = 2, etc
+        if(index == 0):
+            specification_data[page.iloc[0, 0]] = page.iloc[0, 1] # Programe Title
+            specification_data['School of Study'] = page.iloc[2, 1]
+            specification_data['Degree Type'] = page.iloc[3, 1]
+            specification_data['Location'] = page.iloc[9, 1]
+            specification_data['Programme Length'] = page.iloc[10, 1]
+            specification_data['Total Credits'] = page.iloc[11, 1]
+            specification_data['Accredited'] = page.iloc[12, 1]
 
 
-                for index, value in page.iloc[6].items():
-                    if value == 'X':
-                        if index == 1:
-                            specification_data['Attendance Type'] = "Full Time"
-                        elif index == 2:
-                            specification_data['Attendance Type'] = "Part Time"
+            for index, value in page.iloc[6].items():
+                if value == 'X':
+                    if index == 1:
+                        specification_data['Attendance Type'] = "Full Time"
+                    elif index == 2:
+                        specification_data['Attendance Type'] = "Part Time"
 
-                for index, value in page.iloc[8].items():
-                    if value == 'X':
-                        if index == 1:
-                            specification_data['Delivery Pattern'] = "Campus-based"
-                        elif index == 2:
-                            specification_data['Delivery Pattern'] = "Work-based"
-                        elif index == 3:
-                            specification_data['Delivery Pattern'] = "Online/distance"
-
-    else:
-        try:
-            with open(path, 'rb') as f:
-                pdf = pdftotext.PDF(f, raw=True)
-                for index, page in enumerate(pdf):
-                    if(index == 0):
-                        print(page)
-                        # Programme Title
-                        # school/subject title
-                        # final award
-                        # mode of study
-                        # location
-                        # total length of programme
-                        # accredited by
-                        # entry requirements
-        except:
-            print("This PDF file cannot be scraped by our scraper.")
+            for index, value in page.iloc[8].items():
+                if value == 'X':
+                    if index == 1:
+                        specification_data['Delivery Pattern'] = "Campus-based"
+                    elif index == 2:
+                        specification_data['Delivery Pattern'] = "Work-based"
+                    elif index == 3:
+                        specification_data['Delivery Pattern'] = "Online/distance"
 
 
     print(specification_data)
